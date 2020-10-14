ecb8ba06006e3f271905e00e83dcfdbd3ad86f32
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-14 14:37:46 +0100

    Creating a sender component with a scheduled task to send messages thru JMS Template

--- src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -9,12 +9,13 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
+    public static final String MY_QUEUE = "my-hello-world";
+
     @Bean
     public MessageConverter messageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
-
         return converter;
     }
 }
